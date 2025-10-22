76388fc76ce5fb5d77dbc425b4ff33ac30437d0a
Ilya Lapko <ilyshonchik@gmail.com>
2025-10-22 09:25:16 +0200

    New films and delete what i don't need

--- src/main/java/com/example/gallery/controlers/MainControler.java
+++ src/main/java/com/example/gallery/controlers/MainControler.java
@@ -51,6 +51,79 @@ public class MainControler {
         superMan2025.pictureUrl = "https://picsum.photos/203";
         film_list.add(superMan2025);
 
+        ModelFilm homeAlone1990 = new ModelFilm();
+        homeAlone1990.year = 1990;
+        homeAlone1990.comment = "The GOAT new year movie!";
+        homeAlone1990.rating = 8;
+        homeAlone1990.name = "Home alone 1";
+        homeAlone1990.pictureUrl = "https://picsum.photos/203";
+        film_list.add(homeAlone1990);
+
+        ModelFilm homeAlone1992 = new ModelFilm();
+        homeAlone1992.year = 1992;
+        homeAlone1992.comment = "First was better.";
+        homeAlone1992.rating = 7;
+        homeAlone1992.name = "Home alone 2";
+        homeAlone1992.pictureUrl = "https://picsum.photos/203";
+        film_list.add(homeAlone1992);
+
+        ModelFilm homeAlone1997 = new ModelFilm();
+        homeAlone1997.year = 1997;
+        homeAlone1997.comment = "It was the worst.";
+        homeAlone1997.rating = 5;
+        homeAlone1997.name = "Home alone 3";
+        homeAlone1997.pictureUrl = "https://picsum.photos/203";
+        film_list.add(homeAlone1997);
+
+        ModelFilm homeAlone2012 = new ModelFilm();
+        homeAlone2012.year = 2025;
+        homeAlone2012.comment = "";
+        homeAlone2012.rating = 7;
+        homeAlone2012.name = "Home alone 5";
+        homeAlone2012.pictureUrl = "https://picsum.photos/203";
+        film_list.add(homeAlone2012);
+
+        ModelFilm homeAlone2021 = new ModelFilm();
+        homeAlone2021.year = 2021;
+        homeAlone2021.comment = "";
+        homeAlone2021.rating = 3;
+        homeAlone2021.name = "Home alone 6";
+        homeAlone2021.pictureUrl = "https://picsum.photos/203";
+        film_list.add(homeAlone2021);
+
+        ModelFilm greenMile = new ModelFilm();
+        greenMile.year = 1999;
+        greenMile.comment = "All was crying..";
+        greenMile.rating = 9;
+        greenMile.name = "The Green Mile";
+        greenMile.pictureUrl = "https://picsum.photos/203";
+        film_list.add(greenMile);
+
+        ModelFilm bingoBongo1982 = new ModelFilm();
+        bingoBongo1982.year = 1982;
+        bingoBongo1982.comment = "";
+        bingoBongo1982.rating = 9;
+        bingoBongo1982.name = "Bingo bongo";
+        bingoBongo1982.pictureUrl = "https://picsum.photos/203";
+        film_list.add(bingoBongo1982);
+
+        ModelFilm bluff = new ModelFilm();
+        bluff.year = 1976;
+        bluff.comment = "";
+        bluff.rating = 10;
+        bluff.name = "Bluff";
+        bluff.pictureUrl = "https://picsum.photos/203";
+        film_list.add(bluff);
+
+        ModelFilm heIsWorseThanMe1985 = new ModelFilm();
+        heIsWorseThanMe1985.year = 1985;
+        heIsWorseThanMe1985.comment = "Superman in other films was better.";
+        heIsWorseThanMe1985.rating = 9;
+        heIsWorseThanMe1985.name = "He is worse than me";
+        heIsWorseThanMe1985.pictureUrl = "https://picsum.photos/203";
+        film_list.add(heIsWorseThanMe1985);
+
+
         model.addAttribute("film_list", film_list);
         return "film";
     }
