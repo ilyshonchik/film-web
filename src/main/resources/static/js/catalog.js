a8dc8e1d15a108a665185a9dbbb2d3afa78b50e2
DobriyVe4ir <chit.graund@gmail.com>
2026-04-03 18:13:31 +0300

    starting with filters

--- src/main/resources/static/js/catalog.js
+++ src/main/resources/static/js/catalog.js
@@ -1,4 +1,20 @@
 function findByFilters(){
-    let filters = document.URL
-    console.log(filters)
+    let filters = document.getElementById("genres_list")
+    
+    let genres_checkbox = filters.getElementsByClassName("form-check-input")
+    let genres_data = []
+
+    for (let i = 0; i < genres_checkbox.length; i++) {
+        if (genres_checkbox[i].checked) {
+            genres_data.push(genres_checkbox[i].value)
+        }
+    }
+
+    var data = {
+        genres: genres_data,
+        //minimumRating: 2,
+        //sort_by: ""
+    }
+
+    console.log(data)
 }
